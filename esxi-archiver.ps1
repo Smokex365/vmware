1473e227abca3262f791ca9d1f89508933ce74d9
Michael Chamberland <smokex365@gmail.com>
2018-11-27 23:03:03 -0600

    Add files via upload

---
+++ esxi-archiver.ps1
@@ -0,0 +1,31 @@
+#Get Output Type
+#$export_type = Read-Host "Enter 1 to output in ISO format, 2 for Bundle (.zip) format"
+
+#Get-EsxImageProfile -name "ESXi-6.0.*"
+$profile_family = @("ESXi-6.0.0*", "ESXi-6.5.0*")
+$esxi_pf = Get-EsxImageProfile -Name $($profile_family)
+$get_profile = ForEach-Object -Process $esxi_pf
+
+#Specifies Export type, either "1" for ISO or "2" for Bundle (.zip)
+$export_type = 1
+#File output path; current directory
+#$path = Get-Location
+
+#$depot = Add-EsxSoftwareDepot -DepotUrl "https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml"
+
+#Export-EsxImageProfile -ImageProfile “ESXi-6.0.0-2494585-standard” -ExportToBundle -FilePath "ESXi-6.0.0-2494585-standard.zip"
+foreach ($i in $get_profile) {
+    if ($script:export_type = 1) {
+        Write-Host -f Green "Iso" 
+        #Export-EsxImageProfile -ImageProfile "$($image.name)" -ExportToIso -FilePath "$path\$($image.name).iso"
+    }
+    elseif ($script:export_type = 2) {
+        Write-Host -f Blue "Bundle"
+        #Export-EsxImageProfile -ImageProfile "$($image.name)" -ExportToBundle -FilePath "$path\$($image.name).zip"
+    }
+    else {
+        Write-Host -f Red "No Output Type Selected"
+    }
+}
+
+#$depot | Remove-EsxSoftwareDepot
